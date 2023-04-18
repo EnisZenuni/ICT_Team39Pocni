816940ecae5daa40567b815e276b73bda9dfd62c
Lazar Todorov <lazar.todorov.biz@gmail.com>
2023-04-18 22:40:45 +0200

    ICTP-2 ICTP-7 Add Applicant Repo Service and Mutation

--- pocni-api/src/main/java/team39/pocniapi/Service/ApplicantService.java
+++ pocni-api/src/main/java/team39/pocniapi/Service/ApplicantService.java
@@ -1,14 +1,15 @@
 package team39.pocniapi.Service;
 
 import team39.pocniapi.domain.Applicant;
+import team39.pocniapi.domain.ApplicantCreateDto;
 import team39.pocniapi.domain.ApplicantDto;
 
 import java.util.Optional;
 
 public interface ApplicantService {
 
-    ApplicantDto saveApplicant(Applicant applicant);
-
     Optional<ApplicantDto> getApplicant(Long id);
+
+    ApplicantDto create(ApplicantCreateDto applicantCreateDto);
 
 }
--- pocni-api/src/main/java/team39/pocniapi/Service/ApplicantServiceImplementation/ApplicantServiceImplementation.java
+++ pocni-api/src/main/java/team39/pocniapi/Service/ApplicantServiceImplementation/ApplicantServiceImplementation.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import team39.pocniapi.Repository.ApplicantRepository;
 import team39.pocniapi.Service.ApplicantService;
 import team39.pocniapi.domain.Applicant;
+import team39.pocniapi.domain.ApplicantCreateDto;
 import team39.pocniapi.domain.ApplicantDto;
+import team39.pocniapi.domain.ApplicantMapper;
 
 import java.util.Optional;
 
@@ -17,12 +19,13 @@ public class ApplicantServiceImplementation implements ApplicantService {
 
     @Transactional
     @Override
-    public ApplicantDto saveApplicant(Applicant applicant) {
-        return ApplicantDto.toDto(applicantRepository.save(applicant));
+    public ApplicantDto create(ApplicantCreateDto applicantCreateDto) {
+        Applicant applicant = ApplicantMapper.toApplicantFromCreateApplicant(applicantCreateDto);
+        return ApplicantMapper.toDto(applicantRepository.save(applicant));
     }
 
     @Override
     public Optional<ApplicantDto> getApplicant(Long id) {
-        return Optional.of(ApplicantDto.toDto(applicantRepository.findById(id).orElseThrow()));
+        return Optional.of(ApplicantMapper.toDto(applicantRepository.findById(id).orElseThrow()));
     }
 }
