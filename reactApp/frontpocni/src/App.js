e3ab77ce0033912d0dbdbcf94a7a4d1b2bafcfc2
lirimhamiti <lirim.hamiti@students.finki.ukim.mk>
2023-04-30 20:39:27 +0200

    ICTP-3 ICTP-13 Add Basic React Architecture

--- reactApp/frontpocni/src/App.js
+++ reactApp/frontpocni/src/App.js
@@ -1,16 +1,18 @@
 import { ApolloProvider } from '@apollo/client';
 import './App.css';
+import CreateApplicant from './Components/Applicant';
 import { ContextAdmin } from './ContextAdmin/ContextAdmin';
 import Company from './Components/Company';
 import Applicant from './Components/Applicant';
+import MainPage from './Components/MainPage';
+
 
 
 function App() {
   const c = ["test", "test2"]
   return (
     <ContextAdmin.Provider value={c}>
-        <Applicant />
-        <Company/>
+        <MainPage/>
      </ContextAdmin.Provider>
   );
 }
